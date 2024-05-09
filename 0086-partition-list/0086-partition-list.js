3e397b140453d7d4c948487f4ddb14c51f19f58d
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-09 08:02:00 +0530

    Time: 60 ms (59.03%), Space: 50.4 MB (97.13%) - LeetHub

--- 0086-partition-list/0086-partition-list.js
+++ 0086-partition-list/0086-partition-list.js
@@ -13,22 +13,20 @@
 var partition = function (head, x) {
     if (!head) return head;
     let dummy = new ListNode(-1);
+    let greaterList = new ListNode(-1);
     let ptr = dummy;
+    let gptr = greaterList;
     let current = head;
     while (current) {
         if (current.val < x) {
             ptr.next = new ListNode(current.val);
             ptr = ptr.next;
+        } else {
+            gptr.next = new ListNode(current.val);
+            gptr = gptr.next;
         }
         current = current.next;
     }
-    current = head;
-    while (current) {
-        if (current.val >= x) {
-            ptr.next = new ListNode(current.val);
-            ptr = ptr.next;
-        }
-        current = current.next;
-    }
+    ptr.next =  greaterList.next;
     return dummy.next;
 };
