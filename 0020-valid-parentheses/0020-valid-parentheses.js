88f8ab8ee0806e91c8699ea4409789ec32cceefa
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-14 15:04:41 +0530

    Time: 53 ms (79.53%), Space: 49.8 MB (70.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -1,27 +1,4 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function (s) {
-    // let stringArray = s.split("");
-    // let resultStack = [];
-    // for (let x = 0; x < stringArray.length; x++) {
-    //     if (stringArray[x] == '(' || stringArray[x] == '{' || stringArray[x] == '[') {
-    //         resultStack.push(stringArray[x]);
-    //     } else if (stringArray[x] == ')' || stringArray[x] == '}' || stringArray[x] == ']') {
-    //         if (resultStack[resultStack.length - 1] === '(' && stringArray[x] === ')') {
-    //             resultStack.pop();
-    //         } else if (resultStack[resultStack.length - 1] === '{' && stringArray[x] === '}') {
-    //             resultStack.pop();
-    //         } else if (resultStack[resultStack.length - 1] === '[' && stringArray[x] === ']') {
-    //             resultStack.pop();
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    // }
-
-    // return resultStack.length === 0;
     const stack = [];
     const pairs = {
         '(': ')',
@@ -30,11 +7,17 @@ var isValid = function (s) {
     };
 
     for (let x = 0; x < s.length; x++) {
-        if (s[x] === '(' || s[x] === '{' || s[x] === '[') {
-            // TODO pushing element to stack
+        if (s[x] in pairs) {
+            // If it's an opening parenthesis, directly push it onto the stack
             stack.push(s[x]);
-        } else if (s[x] === ')' || s[x] === '}' || s[x] === ']') {
+        } else {
+            // If it's a closing parenthesis
+            if (stack.length === 0) {
+                // If stack is empty, there's no matching opening parenthesis
+                return false;
+            }
             let popedEle = stack.pop();
+            // Check if the popped element matches the current closing parenthesis
             if (pairs[popedEle] !== s[x]) {
                 return false;
             }
