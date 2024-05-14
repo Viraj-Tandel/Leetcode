8fb5a8ce13c90f4d7fc87cc417f1a48290cf41d1
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-14 14:59:24 +0530

    Time: 66 ms (18.03%), Space: 50.1 MB (60.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,23 +3,43 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    let stringArray = s.split("");
-    let resultStack = [];
-    for (let x = 0; x < stringArray.length; x++) {
-        if (stringArray[x] == '(' || stringArray[x] == '{' || stringArray[x] == '[') {
-            resultStack.push(stringArray[x]);
-        } else if (stringArray[x] == ')' || stringArray[x] == '}' || stringArray[x] == ']') {
-            if (resultStack[resultStack.length - 1] === '(' && stringArray[x] === ')') {
-                resultStack.pop();
-            } else if (resultStack[resultStack.length - 1] === '{' && stringArray[x] === '}') {
-                resultStack.pop();
-            } else if (resultStack[resultStack.length - 1] === '[' && stringArray[x] === ']') {
-                resultStack.pop();
-            } else {
+    // let stringArray = s.split("");
+    // let resultStack = [];
+    // for (let x = 0; x < stringArray.length; x++) {
+    //     if (stringArray[x] == '(' || stringArray[x] == '{' || stringArray[x] == '[') {
+    //         resultStack.push(stringArray[x]);
+    //     } else if (stringArray[x] == ')' || stringArray[x] == '}' || stringArray[x] == ']') {
+    //         if (resultStack[resultStack.length - 1] === '(' && stringArray[x] === ')') {
+    //             resultStack.pop();
+    //         } else if (resultStack[resultStack.length - 1] === '{' && stringArray[x] === '}') {
+    //             resultStack.pop();
+    //         } else if (resultStack[resultStack.length - 1] === '[' && stringArray[x] === ']') {
+    //             resultStack.pop();
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    // return resultStack.length === 0;
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let x = 0; x < s.length; x++) {
+        if (s[x] === '(' || s[x] === '{' || s[x] === '[') {
+            // TODO pushing element to stack
+            stack.push(s[x]);
+        } else if (s[x] === ')' || s[x] === '}' || s[x] === ']') {
+            let popedEle = stack.pop();
+            if (pairs[popedEle] !== s[x]) {
                 return false;
             }
         }
     }
 
-    return resultStack.length === 0;
+    return stack.length === 0;
 };
