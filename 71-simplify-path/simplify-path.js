2b7211cce138c53cd8a19fe7d79273c0da0ad928
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-20 11:36:11 +0530

    Time: 68 ms (17.96%) | Memory: 51.1 MB (38.08%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -6,6 +6,7 @@ var simplifyPath = function (path) {
     let splittedPath = path.split("/");
     let pathStack = [];
     let top = -1;
+    splittedPath = filterArray(splittedPath, checkEmptyEle); // * removing empty spaces and unwanted char from array
 
 
     for (let x = 0; splittedPath[x] || splittedPath[x] == ""; x++) {
@@ -22,16 +23,34 @@ var simplifyPath = function (path) {
     let resultString = "";
     let cnt = 0;
 
+    // * counting array length
     while (pathStack[cnt]) {
         cnt++;
-    }
-
-    for (let x = cnt - 1; x >= 0; x--) {
-        resultString = "/" + pathStack[x] + resultString;
     }
 
     if (cnt === 0) {
         resultString = "/"
     }
+
+    // * constructing string by traversing a stack
+    for (let x = cnt - 1; x >= 0; x--) {
+        resultString = "/" + pathStack[x] + resultString;
+    }
+
     return resultString;
 };
+
+// * function to filter array
+function filterArray(arr, callback) {
+    let result = [];
+    for (let x = 0; arr[x] || arr[x] == ""; x++) {
+        if (callback(arr[x]))
+            result.push(arr[x]);
+    }
+    return result;
+}
+
+// * function to check element is empty or not
+function checkEmptyEle(ele) {
+    return (ele === "" || ele === ".") ? false : true;
+}
