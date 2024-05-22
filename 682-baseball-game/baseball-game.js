de3b4ad74f552ddffdfd0f17f18dbb081200ddbf
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-22 12:00:06 +0530

    Time: 53 ms (71.18%) | Memory: 50 MB (43.08%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -8,34 +8,39 @@ var calPoints = function (operations) {
     }
 
     let resultStack = [];
+    let top = -1;
 
     for (let x = 0; operations[x]; x++) {
         if (operations[x] === '+' || operations[x] === 'D' || operations[x] === 'C') {
             // TODO perform operation
             switch (operations[x]) {
                 case 'C':
-                    resultStack.pop();
+                    // resultStack.pop();
+                    resultStack[top] = null;
+                    top--;
                     break;
                 case 'D':
                     resultStack.push(resultStack[resultStack.length - 1] * 2)
                     // resultStack[resultStack.length-1] = resultStack[resultStack.length-1]*2;
+                    let score = resultStack[top] * 2;
+                    resultStack[++top] = score;
                     break;
                 case '+':
-                    let length = resultStack.length;
-                    let score1 = resultStack[length - 1];
-                    let score2 = resultStack[length - 2];
-                    resultStack.push(score1 + score2);
+                    let score1 = resultStack[top];
+                    let score2 = resultStack[top - 1];
+                    resultStack[++top] = score1 + score2;
             }
         } else {
             // TODO push on stack
-            resultStack.push(Number(operations[x]));
+            // resultStack.push(Number(operations[x]));
+            resultStack[++top] = Number(operations[x]);
         }
     }
 
     let sum = 0;
 
     for (let x = resultStack.length - 1; x >= 0; x--) {
-        sum += resultStack[x];
+        if (resultStack[x]) sum += resultStack[x];
     }
 
     return sum;
