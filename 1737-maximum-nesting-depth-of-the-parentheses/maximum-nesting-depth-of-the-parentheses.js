3535bc70b848a2ec110cd8a641be86ec1728eab1
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-22 10:42:00 +0530

    Time: 60 ms (20.41%) | Memory: 51.5 MB (5.61%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -2,22 +2,25 @@
  * @param {string} s
  * @return {number}
  */
-var maxDepth = function(s) {
-    if(!s.length)
+var maxDepth = function (s) {
+    if (!s.length)
         return 0;
 
     let resultStack = [];
     let maxPar = 0;
     let cnt = 0;
-    let deptCnt = 0;
-    while(s[cnt]){
-        if(s[cnt] === '('){
-            resultStack.push('(');
-        } else if(s[cnt] == ')'){
-            maxPar = Math.max(maxPar,resultStack.length);
-            resultStack.pop();
+    let topIndex = -1;
+
+    while (s[cnt]) {
+        if (s[cnt] === '(') {
+            resultStack[++topIndex] = '('
+        } else if (s[cnt] == ')') {
+            maxPar = Math.max(maxPar, topIndex+1);
+            topIndex--;
         }
         cnt++;
     }
+    console.log(resultStack);
+
     return maxPar;
 };
