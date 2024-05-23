a68fe95356e0320bd4c2b3161b2f75097958da13
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-24 00:34:10 +0530

    Time: 41 ms (95.86%) | Memory: 48.5 MB (76.50%) - LeetSync

--- 394-decode-string/decode-string.js
+++ 394-decode-string/decode-string.js
@@ -3,52 +3,57 @@
  * @return {string}
  */
 var decodeString = function (s) {
-  if (s.length === 1) return s;
+    if (s.length === 1) return s;
 
-  let stack = [];
-  for (let x = 0; x < s.length; x++) {
-    if (s[x] !== "]") {
-      stack.push(s[x]);
-    } else {
-      // TODO Decode string
-      let temp = "";
-      for (let y = stack.length - 1; stack[y] !== "["; y--) {
-        temp = stack.pop() + temp;
-      }
-      stack.pop(); // pop out square bracket '['
-      let cnt = "";
-      for (let y = stack.length - 1; y >= 0; y--) {
-        if (!isNaN(stack[y])) cnt = stack.pop() + cnt;
-        else break;
-      }
-      cnt = Number(cnt);
-      let res = repeatString(temp, cnt);
-      stack.push(res);
+    let stack = [];
+    let stackLength = 0;
+    for (let x = 0; s[x]; x++) {
+        if (s[x] !== "]") {
+            stack.push(s[x]);
+        } else {
+            // TODO Decode string
+            let temp = "";
+            stackLength = stack.length;
+            for (let y = stackLength - 1; stack[y] !== "["; y--) {
+                temp = stack.pop() + temp;
+            }
+            stack.pop(); // pop out square bracket '['
+            let cnt = "";
+            // Extract the number
+            stackLength = stack.length;
+            for (let y = stackLength - 1; y >= 0; y--) {
+                if (!isNaN(stack[y])) cnt = stack.pop() + cnt;
+                else break;
+            }
+            cnt = Number(cnt);
+            // Repeat the decoded segment and push back to stack
+            let res = repeatString(temp, cnt);
+            stack.push(res);
+        }
     }
-  }
-  return customJoin(stack, "");
+    return customJoin(stack, "");
 };
 
 // polyfill for Array.prototype.repeat()
 function repeatString(str, count) {
-  let result = "";
-  while (count) {
-    result += str;
-    count--;
-  }
-  return result;
+    let result = "";
+    while (count) {
+        result += str;
+        count--;
+    }
+    return result;
 }
 
 // * polyfill for Array.join()
 function customJoin(arr, separator = ",") {
-  let result = "";
-  for (let i = 0; i < arr.length; i++) {
-    if (i > 0) {
-      result += separator;
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0) {
+            result += separator;
+        }
+        if (arr[i] !== undefined && arr[i] !== null) {
+            result += arr[i];
+        }
     }
-    if (arr[i] !== undefined && arr[i] !== null) {
-      result += arr[i];
-    }
-  }
-  return result;
+    return result;
 }
