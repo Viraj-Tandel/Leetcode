1ff5ef7b3c8c6ff411dc49595f8322b9481080cd
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-24 00:28:05 +0530

    Time: 54 ms (40.43%) | Memory: 48.7 MB (67.79%) - LeetSync

--- 394-decode-string/decode-string.js
+++ 394-decode-string/decode-string.js
@@ -3,42 +3,52 @@
  * @return {string}
  */
 var decodeString = function (s) {
-    if (s.length === 1) return s;
+  if (s.length === 1) return s;
 
-    let stack = [];
-    let finalString = "";
-    for (let x = 0; x < s.length; x++) {
-        if (s[x] !== "]") {
-            stack.push(s[x]);
-        } else {
-            // TODO Decode string
-            let temp = "";
-            for (let y = stack.length - 1; stack[y] !== "["; y--) {
-                temp = stack.pop() + temp;
-            }
-            stack.pop(); // pop out square bracket '['
-            let cnt = "";
-            for (let y = stack.length - 1; y >= 0; y--) {
-                if (!isNaN(stack[y])) cnt = stack.pop() + cnt;
-                else break;
-            }
-            let res = "";
-            cnt = Number(cnt);
-            while (cnt) {
-                res = res + temp;
-                cnt--;
-            }
-            if (stack.length === 0) {
-                finalString = finalString + res;
-            } else {
-                stack.push(res);
-            }
-        }
-    }
-    let temp = "";
-    while (stack.length) {
+  let stack = [];
+  for (let x = 0; x < s.length; x++) {
+    if (s[x] !== "]") {
+      stack.push(s[x]);
+    } else {
+      // TODO Decode string
+      let temp = "";
+      for (let y = stack.length - 1; stack[y] !== "["; y--) {
         temp = stack.pop() + temp;
+      }
+      stack.pop(); // pop out square bracket '['
+      let cnt = "";
+      for (let y = stack.length - 1; y >= 0; y--) {
+        if (!isNaN(stack[y])) cnt = stack.pop() + cnt;
+        else break;
+      }
+      cnt = Number(cnt);
+      let res = repeatString(temp, cnt);
+      stack.push(res);
     }
-    finalString += temp;
-    return finalString;
+  }
+  return customJoin(stack, "");
 };
+
+// polyfill for Array.prototype.repeat()
+function repeatString(str, count) {
+  let result = "";
+  while (count) {
+    result += str;
+    count--;
+  }
+  return result;
+}
+
+// * polyfill for Array.join()
+function customJoin(arr, separator = ",") {
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i > 0) {
+      result += separator;
+    }
+    if (arr[i] !== undefined && arr[i] !== null) {
+      result += arr[i];
+    }
+  }
+  return result;
+}
