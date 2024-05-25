8e92d601c7d68fe65b3ecfc038b485740dbf3f7a
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-05-25 16:54:43 +0530

    Time: 62 ms (98.89%) | Memory: 58.5 MB (62.61%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -11,42 +11,10 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function (headA, headB) {
-
-    if (!headA || !headB) {
-        return null;
+    let a = headA, b = headB
+    while (a !== b) {
+        a = !a ? headB : a.next
+        b = !b ? headA : b.next
     }
-
-    let linkedList1Length = calculateLength(headA);
-    let linkedList2Length = calculateLength(headB);
-    let diff = Math.abs(linkedList1Length - linkedList2Length);
-    let ptr1 = headA;
-    let ptr2 = headB;
-
-    if (linkedList1Length > linkedList2Length) {
-        for (let x = 0; x < diff; x++) {
-            ptr1 = ptr1.next;
-        }
-    } else if (linkedList1Length < linkedList2Length) {
-        for (let x = 0; x < diff; x++) {
-            ptr2 = ptr2.next;
-        }
-    }
-
-    while (ptr1 && ptr2) {
-        if (ptr1 === ptr2) {
-            return ptr1;
-        }
-        ptr1 = ptr1.next;
-        ptr2 = ptr2.next;
-    }
-    return null;
+    return a
 };
-
-function calculateLength(head) {
-    let cnt = 0;
-    while (head) {
-        cnt++;
-        head = head.next;
-    }
-    return cnt;
-}
