723d14464483e491da2c186fe76cc9148197bc8b
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2024-06-07 10:16:45 +0530

    Time: 67 ms (31.69%) | Memory: 50.8 MB (94.12%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -1,19 +1,9 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-    // if (!list1 && !list2)
-    //     return [];
-
     if (!list1)
         return list2;
 
@@ -24,25 +14,21 @@ var mergeTwoLists = function (list1, list2) {
     let ptr = dummy;
     let l1 = list1;
     let l2 = list2;
+
     while (l1 && l2) {
         if (l1.val < l2.val) {
-            let newNode = new ListNode(l1.val);
-            ptr.next = newNode;
-            ptr = ptr.next;
+            ptr.next = l1;
             l1 = l1.next;
         } else {
             let newNode = new ListNode(l2.val);
-            ptr.next = newNode;
-            ptr = ptr.next;
+            ptr.next = l2;
             l2 = l2.next;
         }
+        ptr = ptr.next;
     }
 
-    if (!l1)
-        ptr.next = l2;
-
-    if (!l2)
-        ptr.next = l1;
+    // Attach the remaining part
+    ptr.next = l1 ? l1 : l2;
 
     return dummy.next;
 };
