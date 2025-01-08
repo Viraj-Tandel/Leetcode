339edfaa28458b04ef20f6842031e79ce4cdff0b
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-01-08 08:35:59 +0530

    Time: 3 ms (11.95%) | Memory: 52.1 MB (37.11%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.ts
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.ts
@@ -1,22 +1,7 @@
 function finalValueAfterOperations(operations: string[]): number {
     let numValue = 0;
     for (let x = 0; operations[x]; x++) {
-        switch (operations[x]) {
-            case 'X++':
-                numValue += 1;
-                break;
-
-            case '++X':
-                numValue += 1;
-                break;
-
-            case '--X':
-                numValue -= 1;
-                break;
-
-            case 'X--':
-                numValue -= 1;
-        }
+        numValue += (operations[x][1] === '+') ? 1 : -1;
     }
     return numValue;
 };
