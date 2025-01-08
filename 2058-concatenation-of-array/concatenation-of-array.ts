a751f26031f0c4f71a52595e7834ced0c35f761e
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-01-08 08:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (15.61%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.ts
+++ 2058-concatenation-of-array/concatenation-of-array.ts
@@ -1,9 +1,20 @@
 function getConcatenation(nums: number[]): number[] {
-    let ans = [...nums];
+    // Initialize an empty array to store the concatenated result
+    let ans = [];
 
-    for (let x = 0;nums[x]; x++) {
-        ans.push(nums[x]);
+    // Store the length of the input array
+    let length = nums.length;
+
+    // Iterate through the input array
+    for (let x = 0; x < length; x++) {
+        // Copy the current element of nums to the corresponding position in the first half of ans
+        ans[x] = nums[x];
+
+        // Copy the current element of nums to the corresponding position in the second half of ans
+        // By using ans[x + length], we directly assign the value, avoiding dynamic resizing
+        ans[x + length] = nums[x];
     }
 
+    // Return the concatenated array
     return ans;
 };
