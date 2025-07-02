7b6e8fc30b4a1cb401b895e1a1177f54212d49c3
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-03 00:17:05 +0530

    Time: 5 ms (15.39%) | Memory: 57.9 MB (50.73%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (nums.length === 0 || nums.length === 1) {
+        return nums;
+    }
+
+    let uniqueIndex = 0;
+
+    for (let x = 0; x < nums.length; x++) {
+        if (nums[uniqueIndex] !== nums[x]) {
+            nums[uniqueIndex + 1] = nums[x];
+            uniqueIndex++;
+        }
+    }
+    console.log(uniqueIndex)
+    return uniqueIndex + 1;
+};
