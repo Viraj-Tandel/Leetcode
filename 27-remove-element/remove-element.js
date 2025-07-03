f76ff5f3dd6aeb977fcc3df89e798f71ffe575e7
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-04 00:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (49.06%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -7,13 +7,14 @@ var removeElement = function(nums, val) {
     if(nums.length === 0) 
         return 0;
 
-    let iterator;
-    for(iterator = 0; iterator < nums.length; iterator++){
-        if(nums[iterator] == val){
-            nums.splice(iterator,1);
-            iterator--;
+    let i =0;
+    
+    for(let j = 0;j < nums.length ; j++){
+        if(nums[j] !== val){
+            nums[i] = nums[j];
+            i++;
         }
     }
 
-    return iterator;
+    return i;
 };
