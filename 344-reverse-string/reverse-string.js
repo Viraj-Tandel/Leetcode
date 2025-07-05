7e5e5627c6aed8938361d7ee6e719afadf45b788
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-05 11:57:00 +0530

    Time: 6 ms (6.76%) | Memory: 62.2 MB (15.98%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -2,12 +2,13 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
+var reverseString = function (s) {
     let size = s.length;
-    let temp;
-    for(let i = 0; i < Math.floor(size/2); i++){
-        temp = s[i];
-        s[i] = s[size-1-i];
-        s[size-1-i] = temp;
+    // let temp;
+    for (let i = 0; i < Math.floor(size / 2); i++) {
+        // temp = s[i];
+        // s[i] = s[size-1-i];
+        // s[size-1-i] = temp;
+        [s[i], s[size - 1 - i]] = [s[size - 1 - i], s[i]];
     }
 };
