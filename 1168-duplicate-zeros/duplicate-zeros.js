5408afef916d2dd8727cba9eae74876b92ae088e
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-20 23:06:16 +0530

    Time: 3 ms (66.94%) | Memory: 58.2 MB (12.02%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.js
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -3,11 +3,23 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function (arr) {
+    // for (let x = 0; x < arr.length; x++) {
+    //     if (arr[x] == 0) {
+    //         arr.splice(x, 0, 0);
+    //         arr.splice(arr.length - 1, 1);
+    //         x++;
+    //     }
+    // }
+
+    const temp = [];
     for (let x = 0; x < arr.length; x++) {
+        temp.push(arr[x]);
         if (arr[x] == 0) {
-            arr.splice(x, 0, 0);
-            arr.splice(arr.length - 1, 1);
-            x++;
+            temp.push(0);
         }
+    }
+
+    for (let x = 0; x < arr.length; x++) {
+        arr[x] = temp[x]
     }
 };
