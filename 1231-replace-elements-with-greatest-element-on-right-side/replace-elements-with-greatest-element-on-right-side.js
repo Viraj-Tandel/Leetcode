2344d6ef3aefaf83f5327e5a024a19cfb5178aa9
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-24 20:19:43 +0530

    Time: 2 ms (57.14%) | Memory: 64.1 MB (71.64%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -4,20 +4,33 @@
  */
 var replaceElements = function (arr) {
 
+    // if (arr.length <= 1) return [-1];
+
+    // let result = [];
+
+    // for (let x = 0; x < arr.length - 1; x++) {
+    //     let max = -Infinity;
+    //     for (let y = x + 1; y < arr.length; y++) {
+    //         if (max < arr[y]) {
+    //             max = arr[y];
+    //         }
+    //     }
+    //     result.push(max);
+    // }
+    // result.push(-1);
+
+    // return result;
+
     if (arr.length <= 1) return [-1];
 
-    let result = [];
-
-    for (let x = 0; x < arr.length - 1; x++) {
-        let max = -Infinity;
-        for (let y = x + 1; y < arr.length; y++) {
-            if (max < arr[y]) {
-                max = arr[y];
-            }
+    let max = -1;
+    for (let x = arr.length - 1; x >= 0; x--) {
+        let current = arr[x];
+        arr[x] = max;
+        if (max < current) {
+            max = current;
         }
-        result.push(max);
     }
-    result.push(-1);
 
-    return result;
+    return arr;
 };
