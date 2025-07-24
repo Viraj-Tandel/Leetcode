92eae9f2589608c090b5741c41fc686d127a5518
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-24 21:59:14 +0530

    Time: 1 ms (97.81%) | Memory: 57 MB (60.47%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -5,19 +5,37 @@
  */
 var nextGreaterElement = function (nums1, nums2) {
 
-    for (let x = 0; x < nums1.length; x++) {
-        let elementFound = false;
-        let max = -1;
-        for (let y = 0; y < nums2.length; y++) {
-            if (!elementFound && nums2[y] == nums1[x]) {
-                elementFound = true;
-            }
-            if (elementFound && nums2[y] > nums1[x]) {
-                max = nums2[y];
-                break;
-            }
+    // for (let x = 0; x < nums1.length; x++) {
+    //     let elementFound = false;
+    //     let max = -1;
+    //     for (let y = 0; y < nums2.length; y++) {
+    //         if (!elementFound && nums2[y] == nums1[x]) {
+    //             elementFound = true;
+    //         }
+    //         if (elementFound && nums2[y] > nums1[x]) {
+    //             max = nums2[y];
+    //             break;
+    //         }
+    //     }
+    //     nums1[x] = max;
+    // }
+    // return nums1;
+
+    let stack = [];
+    let nextGreaterMap = new Map();
+
+    for (let x = nums2.length - 1; x >= 0; x--) {
+        let current = nums2[x];
+        // Maintain a decreasing stack (pop smaller or equal elements)
+        while (stack.length > 0 && current > stack[stack.length - 1]) {
+            stack.pop();
         }
-        nums1[x] = max;
+        // Top of stack is next greater if stack is not empty
+        let nextGreater = stack.length === 0 ? -1 : stack[stack.length - 1];
+        nextGreaterMap.set(current, nextGreater);
+        stack.push(current);
     }
-    return nums1;
+    // Build the result using the map for nums1
+    let result = nums1.map(num => nextGreaterMap.get(num));
+    return result;
 };
