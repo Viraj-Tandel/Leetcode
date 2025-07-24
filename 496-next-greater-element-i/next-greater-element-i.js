151dc70df4cd2190ac38b2c9e79d850d4518ce41
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-24 20:44:21 +0530

    Time: 4 ms (56.67%) | Memory: 56.4 MB (71.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,40 +4,20 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-    if (nums1.length === 0 || nums2.length === 0) {
-        return;
-    }
 
-    let stack = [];
-    let result = [];
-    let size = nums2.length - 1;
-    let top = -1;
-    let map = {};
-
-    for (let x = size; x >= 0; x--) {
-        // Pop elements from stack that are less than or equal to the current element
-        while (top !== -1 && stack[top] <= nums2[x]) {
-            top--;
+    for (let x = 0; x < nums1.length; x++) {
+        let elementFound = false;
+        let max = -1;
+        for (let y = 0; y < nums2.length; y++) {
+            if (!elementFound && nums2[y] == nums1[x]) {
+                elementFound = true;
+            }
+            if (elementFound && nums2[y] > nums1[x]) {
+                max = nums2[y];
+                break;
+            }
         }
-
-        // If stack is not empty, the top element is the next greater element
-        if (top !== -1) {
-            result[x] = stack[top];
-        } else {
-            result[x] = -1;
-        }
-
-        // Push current element to the stack
-        stack[++top] = nums2[x];
-
-        // Map the current element to its next greater element
-        map[nums2[x]] = result[x];
+        nums1[x] = max;
     }
-    result = [];
-
-    for (let x = 0; nums1[x] || nums1[x] === 0; x++) {
-        result[x] = map[nums1[x]];
-    }
-
-    return result;
+    return nums1;
 };
