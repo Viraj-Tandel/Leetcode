d6cb78b22bd304ecae041843194c090987743b98
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-26 14:45:02 +0530

    Time: 1 ms (78.53%) | Memory: 53.9 MB (72.94%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -3,15 +3,24 @@
  * @return {boolean}
  */
 var checkIfExist = function (arr) {
+    // for (let x = 0; x < arr.length; x++) {
+    //     let doubleNum = 2 * arr[x];
+    //     for (let y = 0; y < arr.length; y++) {
+    //         if (x !== y) {
+    //             if (doubleNum == arr[y]) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    // }
+    // return false;
+
+    let resultMap = new Set();
     for (let x = 0; x < arr.length; x++) {
-        let doubleNum = 2 * arr[x];
-        for (let y = 0; y < arr.length; y++) {
-            if (x !== y) {
-                if (doubleNum == arr[y]) {
-                    return true;
-                }
-            }
+        if (resultMap.has(arr[x] * 2) || (arr[x] % 2 == 0 && resultMap.has(arr[x] / 2))) {
+            return true;
         }
+        resultMap.add(arr[x]);
     }
     return false;
 };
