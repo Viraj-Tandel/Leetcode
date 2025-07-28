2343dac067948d72b895209e88c99af1fe4f4fbf
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-28 11:48:56 +0530

    Time: 45 ms (7.36%) | Memory: 72 MB (83.14%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+
+    // *** Brute Force Approch ----> TLE
+
+    // let result = [];
+    // for (let x = 1; x <= nums.length; x++) {
+    //     if (nums.indexOf(x) == -1) {
+    //         result.push(x);
+    //     }
+    // }
+    // return result;
+
+    let result = [];
+    let resultMap = {};
+
+    // * build a result map
+    for (let x = 0; x < nums.length; x++) {
+        if (resultMap[nums[x]]) {
+            resultMap[nums[x]] = resultMap[nums[x]] + 1;
+        } else {
+            resultMap[nums[x]] = 1;
+        }
+    }
+    // build result set
+    for (let x = 1; x <= nums.length; x++) {
+        if (!resultMap[x]) {
+            result.push(x);
+        }
+    }
+
+    return result;
+};
