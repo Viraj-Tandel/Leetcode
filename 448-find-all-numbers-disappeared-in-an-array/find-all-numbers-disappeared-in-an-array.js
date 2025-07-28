fe81ec3940d09ccfcb9c8d0407f4ff6558e79179
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-28 12:32:51 +0530

    Time: 4 ms (98.81%) | Memory: 72.6 MB (70.35%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -14,23 +14,36 @@ var findDisappearedNumbers = function (nums) {
     // }
     // return result;
 
-    let result = [];
-    let resultMap = {};
+    // let result = [];
+    // let resultMap = {};
 
-    // * build a result map
+    // // * build a result map
+    // for (let x = 0; x < nums.length; x++) {
+    //     if (resultMap[nums[x]]) {
+    //         resultMap[nums[x]] = resultMap[nums[x]] + 1;
+    //     } else {
+    //         resultMap[nums[x]] = 1;
+    //     }
+    // }
+    // // build result set
+    // for (let x = 1; x <= nums.length; x++) {
+    //     if (!resultMap[x]) {
+    //         result.push(x);
+    //     }
+    // }
+
+    // return result;
+
     for (let x = 0; x < nums.length; x++) {
-        if (resultMap[nums[x]]) {
-            resultMap[nums[x]] = resultMap[nums[x]] + 1;
-        } else {
-            resultMap[nums[x]] = 1;
-        }
+        let index = Math.abs(nums[x]) - 1;
+        nums[index] = -Math.abs(nums[index]);
     }
-    // build result set
-    for (let x = 1; x <= nums.length; x++) {
-        if (!resultMap[x]) {
-            result.push(x);
-        }
-    }
+    let result = [];
 
+    for (let x = 0; x < nums.length; x++) {
+        if (nums[x] > 0) {
+            result.push(x + 1);
+        }
+    }
     return result;
 };
