73408625fad9f8d6e65c8b2fef9772b39674879b
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-07-30 23:30:22 +0530

    Time: 6 ms (55.55%) | Memory: 55 MB (73.29%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+    let frequancyMap = {};
+
+    for (let x = 0; x < s.length; x++) {
+        if (frequancyMap[s[x]]) {
+            frequancyMap[s[x]] = frequancyMap[s[x]] + 1;
+        } else {
+            frequancyMap[s[x]] = 1;
+        }
+    }
+
+    // console.log("frequancyMap: ", frequancyMap);
+    for (let x = 0; x < t.length; x++) {
+        if (!frequancyMap.hasOwnProperty(t[x]) || frequancyMap[t[x]] === 0) {
+            return t[x];
+        } else {
+            frequancyMap[t[x]] = frequancyMap[t[x]] - 1;
+        }
+    }
+
+};
