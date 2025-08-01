68028252e65935ab85f88832e0e6dd6dc0d40832
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-01 06:57:15 +0530

    Time: 1 ms (84.14%) | Memory: 53.6 MB (95.66%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+    let resultMap = {};
+    let result = [];
+
+    for (let x = 0; x < nums1.length; x++) {
+        if (resultMap[nums1[x]]) {
+            ++resultMap[nums1[x]];
+        } else {
+            resultMap[nums1[x]] = 1;
+        }
+    }
+
+    for (let x = 0; x < nums2.length; x++) {
+        if (resultMap[nums2[x]] > 0) {
+            result.push(nums2[x]);
+            resultMap[nums2[x]]--;
+        }
+    }
+
+    return result;
+};
