e774a7d72cab67a3037157c74003e7ae89ec3a5b
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-01 06:42:15 +0530

    Time: 1 ms (80.95%) | Memory: 55.2 MB (42.03%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,31 +4,43 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-    nums1 = [...new Set(nums1)];
-    nums2 = [...new Set(nums2)];
+    // nums1 = [...new Set(nums1)];
+    // nums2 = [...new Set(nums2)];
 
-    let resultMap = {};
+    // let resultMap = {};
 
-    for (let x = 0; x < nums1.length; x++) {
-        if (resultMap[nums1[x]]) {
-            resultMap[nums1[x]]++;
-        } else {
-            resultMap[nums1[x]] = 1;
-        }
-    }
+    // for (let x = 0; x < nums1.length; x++) {
+    //     if (resultMap[nums1[x]]) {
+    //         resultMap[nums1[x]]++;
+    //     } else {
+    //         resultMap[nums1[x]] = 1;
+    //     }
+    // }
 
-    for (let x = 0; x < nums2.length; x++) {
-        if (resultMap[nums2[x]]) {
-            resultMap[nums2[x]]++;
-        } else {
-            resultMap[nums2[x]] = 1;
-        }
-    }
+    // for (let x = 0; x < nums2.length; x++) {
+    //     if (resultMap[nums2[x]]) {
+    //         resultMap[nums2[x]]++;
+    //     } else {
+    //         resultMap[nums2[x]] = 1;
+    //     }
+    // }
 
+    // let result = [];
+    // for (let x in resultMap) {
+    //     if (resultMap[x] > 1) {
+    //         result.push(Number(x));
+    //     }
+    // }
+
+    // return result;
+
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
     let result = [];
-    for (let x in resultMap) {
-        if (resultMap[x] > 1) {
-            result.push(Number(x));
+
+    for (let num of set1) {
+        if (set2.has(num)) {
+            result.push(num);
         }
     }
 
