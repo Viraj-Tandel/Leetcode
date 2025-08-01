fad9526b8e09a385f8851f01bdebbba16c19f1f6
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-01 08:14:36 +0530

    Time: 12 ms (72.54%) | Memory: 54.7 MB (98.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,38 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
+    // if (s.length !== t.length) {
+    //     return false;
+    // }
+
+    // let sMap = {};
+    // let tMap = {};
+
+    // for (let x = 0; x < t.length; x++) {
+    //     if (sMap[s[x]]) {
+    //         sMap[s[x]]++;
+    //     } else {
+    //         sMap[s[x]] = 1;
+    //     }
+
+    //     if (tMap[t[x]]) {
+    //         tMap[t[x]]++;
+    //     } else {
+    //         tMap[t[x]] = 1;
+    //     }
+    // }
+
+    // for (let x = 0; x < t.length; x++) {
+    //     if (tMap[t[x]] && sMap[t[x]]) {
+    //         tMap[t[x]]--;
+    //         sMap[t[x]]--;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+
     if (s.length !== t.length) {
         return false;
     }
