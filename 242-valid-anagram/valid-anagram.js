4bcd6c60dad3511a660c0f84344e891930ba9265
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-01 08:13:26 +0530

    Time: 8 ms (89.16%) | Memory: 57.1 MB (69.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -8,30 +8,15 @@ var isAnagram = function (s, t) {
         return false;
     }
 
-    let sMap = {};
-    let tMap = {};
+    let freqMap = {};
 
-    for (let x = 0; x < t.length; x++) {
-        if (sMap[s[x]]) {
-            sMap[s[x]]++;
-        } else {
-            sMap[s[x]] = 1;
-        }
-
-        if (tMap[t[x]]) {
-            tMap[t[x]]++;
-        } else {
-            tMap[t[x]] = 1;
-        }
+    for (let x = 0; x < s.length; x++) {
+        freqMap[s[x]] = (freqMap[s[x]] || 0) + 1;
     }
 
     for (let x = 0; x < t.length; x++) {
-        if (tMap[t[x]] && sMap[t[x]]) {
-            tMap[t[x]]--;
-            sMap[t[x]]--;
-        } else {
-            return false;
-        }
+        if (!freqMap[t[x]]) return false;
+        freqMap[t[x]]--;
     }
 
     return true;
