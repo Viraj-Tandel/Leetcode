29b5adf95e84e580bf0587bc7c1b8d98bce081f3
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-02 11:46:13 +0530

    Time: 12 ms (72.60%) | Memory: 57 MB (71.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -63,7 +63,7 @@ var isAnagram = function (s, t) {
     for (let x = 0; x < s.length; x++) {
         freqMap[s[x]] = (freqMap[s[x]] || 0) + 1;
     }
-    console.log(freqMap)
+
     for (let x = 0; x < t.length; x++) {
         if (freqMap[t[x]]) {
             freqMap[t[x]]--;
