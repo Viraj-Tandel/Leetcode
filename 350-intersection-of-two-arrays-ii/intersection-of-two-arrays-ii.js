1772728a72921038cc80f039a877e082e701c833
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-02 11:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (84.57%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -4,21 +4,37 @@
  * @return {number[]}
  */
 var intersect = function (nums1, nums2) {
-    let resultMap = {};
-    let result = [];
+    // let resultMap = {};
+    // let result = [];
+
+    // for (let x = 0; x < nums1.length; x++) {
+    //     if (resultMap[nums1[x]]) {
+    //         ++resultMap[nums1[x]];
+    //     } else {
+    //         resultMap[nums1[x]] = 1;
+    //     }
+    // }
+
+    // for (let x = 0; x < nums2.length; x++) {
+    //     if (resultMap[nums2[x]] > 0) {
+    //         result.push(nums2[x]);
+    //         resultMap[nums2[x]]--;
+    //     }
+    // }
+
+    // return result;
+
+    let freqMap = {};
 
     for (let x = 0; x < nums1.length; x++) {
-        if (resultMap[nums1[x]]) {
-            ++resultMap[nums1[x]];
-        } else {
-            resultMap[nums1[x]] = 1;
-        }
+        freqMap[nums1[x]] = (freqMap[nums1[x]] || 0) + 1;
     }
 
+    let result = [];
     for (let x = 0; x < nums2.length; x++) {
-        if (resultMap[nums2[x]] > 0) {
+        if (freqMap[nums2[x]]) {
+            --freqMap[nums2[x]];
             result.push(nums2[x]);
-            resultMap[nums2[x]]--;
         }
     }
 
