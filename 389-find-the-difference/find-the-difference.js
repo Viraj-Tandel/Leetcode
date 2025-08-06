095ebdd36e9b11a2a1558c88243f92a99073a0aa
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-06 21:40:23 +0530

    Time: 6 ms (54.56%) | Memory: 54.5 MB (86.54%) - LeetSync

--- 389-find-the-difference/find-the-difference.js
+++ 389-find-the-difference/find-the-difference.js
@@ -4,36 +4,36 @@
  * @return {character}
  */
 var findTheDifference = function (s, t) {
-    // let frequancyMap = {};
-
-    // for (let x = 0; x < s.length; x++) {
-    //     if (frequancyMap[s[x]]) {
-    //         frequancyMap[s[x]] = frequancyMap[s[x]] + 1;
-    //     } else {
-    //         frequancyMap[s[x]] = 1;
-    //     }
-    // }
-
-    // // console.log("frequancyMap: ", frequancyMap);
-    // for (let x = 0; x < t.length; x++) {
-    //     if (!frequancyMap.hasOwnProperty(t[x]) || frequancyMap[t[x]] === 0) {
-    //         return t[x];
-    //     } else {
-    //         frequancyMap[t[x]] = frequancyMap[t[x]] - 1;
-    //     }
-    // }
-
-    let sumS = 0;
-    let sumT = 0;
+    let frequancyMap = {};
 
     for (let x = 0; x < s.length; x++) {
-        sumS += s.charCodeAt(x);
+        if (frequancyMap[s[x]]) {
+            frequancyMap[s[x]] = frequancyMap[s[x]] + 1;
+        } else {
+            frequancyMap[s[x]] = 1;
+        }
     }
 
-    for (let y = 0; y < t.length; y++) {
-        sumT += t.charCodeAt(y);
+    // console.log("frequancyMap: ", frequancyMap);
+    for (let x = 0; x < t.length; x++) {
+        if (!frequancyMap.hasOwnProperty(t[x]) || frequancyMap[t[x]] === 0) {
+            return t[x];
+        } else {
+            frequancyMap[t[x]] = frequancyMap[t[x]] - 1;
+        }
     }
 
-    return String.fromCharCode(sumT - sumS);
+    // let sumS = 0;
+    // let sumT = 0;
+
+    // for (let x = 0; x < s.length; x++) {
+    //     sumS += s.charCodeAt(x);
+    // }
+
+    // for (let y = 0; y < t.length; y++) {
+    //     sumT += t.charCodeAt(y);
+    // }
+
+    // return String.fromCharCode(sumT - sumS);
 
 };
