2d51831ecd4ea0e1132834197d336f18bab293fe
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-08 07:24:40 +0530

    Time: 2 ms (13.91%) | Memory: 55.2 MB (7.85%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+    let left = 1;
+    let right = num;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if ((mid * mid) == num) {
+            return true;
+        } else if (mid * mid > num) {
+            right = mid - 1;
+        } else {
+            left = left + 1;
+        }
+    }
+
+    return false;
+};
