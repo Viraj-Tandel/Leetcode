03053a8849f89996fcfada2844577ee4c5cc36b5
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-08 06:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (6.59%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // we compare middle with its next element
+        if (nums[mid] > nums[mid + 1]) {
+            // Peak is on the left side (including mid)
+            right = mid;
+        } else {
+            // Peak is on the right side (excluding mid)
+            left = mid + 1;
+        }
+    }
+    return left;
+};
