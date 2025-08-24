33d4bd7855546e61b9528f091f4cf17c083c87dc
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-24 22:02:17 +0530

    Time: 2 ms (41.01%) | Memory: 54.7 MB (95.97%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x < 2) {
+        return x;
+    }
+    let left = 1;
+    let right = x;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+        if (square == x) {
+            return mid;
+        } else if (square > x) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return right;
+};
