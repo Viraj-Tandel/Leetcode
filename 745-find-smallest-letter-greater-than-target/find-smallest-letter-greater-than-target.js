2ec6345dc041dfbb3b100b626459946298b0d135
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-25 22:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (65.85%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -4,21 +4,39 @@
  * @return {character}
  */
 var nextGreatestLetter = function (letters, target) {
+    // let left = 0;
+    // let right = letters.length - 1;
+    // let targetCode = target.charCodeAt(0);
+
+    // while (left < right) {
+    //     let mid = Math.floor((left + right) / 2);
+    //     let temp = letters[mid].charCodeAt(0);
+
+    //     if (temp > targetCode) {
+    //         right = mid;
+    //     } else {
+    //         left = mid + 1;
+    //     }
+    // }
+    // return letters[right].charCodeAt(0) > targetCode
+    //     ? letters[right]
+    //     : letters[0];
+
+    // **** PRACTISE 25/8/2025
     let left = 0;
     let right = letters.length - 1;
-    let targetCode = target.charCodeAt(0);
+    let targetAsciie = target.charCodeAt(0);
 
     while (left < right) {
         let mid = Math.floor((left + right) / 2);
-        let temp = letters[mid].charCodeAt(0);
+        let midAsciieValue = letters[mid].charCodeAt(0);
 
-        if (temp > targetCode) {
+        if (midAsciieValue > targetAsciie) {
             right = mid;
         } else {
             left = mid + 1;
         }
     }
-    return letters[right].charCodeAt(0) > targetCode
-        ? letters[right]
-        : letters[0];
+
+    return letters[right].charCodeAt(0) > targetAsciie ? letters[right] : letters[0];
 };
