3fffe2094eff3319594c940e324c09f1941016f3
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-27 01:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (59.28%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,34 +3,50 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-    const binarySearch = (nums, target, isSearchingLeft) => {
-        let left = 0;
-        let right = nums.length - 1;
-        let idx = -1;
-      
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
+var searchRange = function (nums, target) {
+    if (nums.length == 0) {
+        return [-1, -1];
+    }
+    if (nums.length == 1) {
+        return nums[0] == target ? [0, 0] : [-1, -1];
+    }
+
+    let firstIndex = -1;
+    let lastIndex = -1;
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] == target) {
+            firstIndex = mid
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-      
-        return idx;
-    };
-    
-    const left = binarySearch(nums, target, true);
-    const right = binarySearch(nums, target, false);
-    
-    return [left, right];    
+    }
+
+    left = 0;
+    right = nums.length - 1;
+
+    if (firstIndex == -1)
+        return [-1, -1];
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] == target) {
+            lastIndex = mid
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return [firstIndex, lastIndex];
 };
