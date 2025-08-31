5ed5ece652104bc276c9587283d836c4393a8aa2
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-08-31 21:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (93.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,6 +4,32 @@
  * @return {number}
  */
 var search = function (nums, target) {
+    // let left = 0;
+    // let right = nums.length - 1;
+
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+
+    //     if (nums[mid] == target) {
+    //         return mid;
+    //     } else if (nums[left] <= nums[mid]) {
+    //         if (nums[left] <= target && target <= nums[mid]) {
+    //             right = mid - 1;
+    //         } else {
+    //             left = mid + 1;
+    //         }
+    //     } else {
+    //         if (nums[mid] <= target && target <= nums[right]) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid - 1;
+    //         }
+    //     }
+    // }
+    // return -1;
+
+    // * practise solve 31/8/2025
+
     let left = 0;
     let right = nums.length - 1;
 
@@ -26,5 +52,6 @@ var search = function (nums, target) {
             }
         }
     }
+
     return -1;
 };
