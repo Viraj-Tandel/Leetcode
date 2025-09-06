03aa18ba745acb0f4fbdf36d4bc4dbce1f8ae85e
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-09-06 21:15:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (8.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,23 +4,69 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
+    // if (nums.length == 0) {
+    //     return [-1, -1];
+    // }
+    // if (nums.length == 1) {
+    //     return nums[0] == target ? [0, 0] : [-1, -1];
+    // }
+
+    // let firstIndex = -1;
+    // let lastIndex = -1;
+    // let left = 0;
+    // let right = nums.length - 1;
+
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+
+    //     if (nums[mid] == target) {
+    //         firstIndex = mid
+    //         right = mid - 1;
+    //     } else if (nums[mid] < target) {
+    //         left = mid + 1;
+    //     } else {
+    //         right = mid - 1;
+    //     }
+    // }
+
+    // left = 0;
+    // right = nums.length - 1;
+
+    // if (firstIndex == -1)
+    //     return [-1, -1];
+
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+
+    //     if (nums[mid] == target) {
+    //         lastIndex = mid
+    //         left = mid + 1;
+    //     } else if (nums[mid] < target) {
+    //         left = mid + 1;
+    //     } else {
+    //         right = mid - 1;
+    //     }
+    // }
+
+    // return [firstIndex, lastIndex];
+
+
+    // * PRACTISE RESOLVE -----> 6/9/2025
+
     if (nums.length == 0) {
         return [-1, -1];
-    }
-    if (nums.length == 1) {
-        return nums[0] == target ? [0, 0] : [-1, -1];
     }
 
     let firstIndex = -1;
     let lastIndex = -1;
+
     let left = 0;
     let right = nums.length - 1;
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-
         if (nums[mid] == target) {
-            firstIndex = mid
+            firstIndex = mid;
             right = mid - 1;
         } else if (nums[mid] < target) {
             left = mid + 1;
@@ -32,14 +78,11 @@ var searchRange = function (nums, target) {
     left = 0;
     right = nums.length - 1;
 
-    if (firstIndex == -1)
-        return [-1, -1];
-
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
 
         if (nums[mid] == target) {
-            lastIndex = mid
+            lastIndex = mid;
             left = mid + 1;
         } else if (nums[mid] < target) {
             left = mid + 1;
