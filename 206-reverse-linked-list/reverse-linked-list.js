bac1d3f2ade1da37b69d97b3d10b3a8bdde4cd84
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-10-27 23:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (35.04%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -9,16 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {
+var reverseList = function(head) {
     let prev = null;
     let current = head;
 
-    while (current) {
+    while(current){
         let temp = current.next;
         current.next = prev;
         prev = current;
         current = temp;
     }
-
     return prev;
 };
