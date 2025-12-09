946d7a2cdb43da963aab5ac278bccea86193f9a9
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-12-09 20:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 57.9 MB (48.61%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -10,12 +10,14 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-    if (!head) return head;
 
+    if (!head) {
+        return head;
+    }
     let current = head;
 
     while (current && current.next) {
-        if (current.val === current.next.val) {
+        if (current.val == current.next.val) {
             current.next = current.next.next;
         } else {
             current = current.next;
