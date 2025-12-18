7647beddd21ecb242671abe1939e91114fdc480d
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-12-18 22:18:46 +0530

    Time: 9 ms (70.87%) | Memory: 63.4 MB (84.25%) - LeetSync

---
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var nextLargerNodes = function (head) {
+
+    if (!head) {
+        return []
+    }
+
+    let nums = [];
+    let current = head;
+
+    while (current) {
+        nums.push(current.val);
+        current = current.next;
+    }
+
+    let ans = new Array(nums.length).fill(0);
+    let stack = [];
+    for (let x = nums.length - 1; x >= 0; x--) {
+        while (stack.length && stack[stack.length - 1] <= nums[x]) {
+            stack.pop();
+        }
+        ans[x] = stack.length ? stack[stack.length - 1] : 0;
+        stack.push(nums[x]);
+    }
+
+    return ans;
+};
