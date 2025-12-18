28a1f6873e4a2ee7ad111f42a9c57f16b0b727f4
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-12-18 22:27:36 +0530

    Time: 9 ms (70.87%) | Memory: 64.4 MB (33.86%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.js
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.js
@@ -9,29 +9,25 @@
  * @param {ListNode} head
  * @return {number[]}
  */
-var nextLargerNodes = function(head) {
-
-    if(!head){
-        
-    }
+var nextLargerNodes = function (head) {
 
     let nums = [];
     let current = head;
 
-    while(current){
+    while (current) {
         nums.push(current.val);
         current = current.next;
     }
 
-    let ans = new Array(nums.length-1).fill(0);
+    let ans = new Array(nums.length - 1).fill(0);
     let stack = [];
 
-    for(let x = nums.length - 1; x >= 0; x--){
-        while(stack.length && stack[stack.length - 1]<= nums[x]){
+    for (let x = nums.length - 1; x >= 0; x--) {
+        while (stack.length && stack[stack.length - 1] <= nums[x]) {
             stack.pop();
         }
 
-        ans[x] = stack.length?stack[stack.length-1]:0;
+        ans[x] = stack.length ? stack[stack.length - 1] : 0;
         stack.push(nums[x]);
     }
 
