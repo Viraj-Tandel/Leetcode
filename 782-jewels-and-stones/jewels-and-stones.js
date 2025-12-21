a97eb216165fdc19871aabdf71e0560527dcab62
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-12-21 14:46:28 +0530

    Time: 1 ms (33.26%) | Memory: 54.5 MB (74.58%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -6,12 +6,22 @@
 var numJewelsInStones = function (jewels, stones) {
     let count = 0;
 
+    //     for (let x = 0; x < stones.length; x++) {
+    //         for (let y = 0; y < jewels.length; y++) {
+    //             if (stones[x] == jewels[y]) {
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+
+    let jSet = new Set(jewels);
+
     for (let x = 0; x < stones.length; x++) {
-        for (let y = 0; y < jewels.length; y++) {
-            if (stones[x] == jewels[y]) {
-                count++;
-            }
+        if (jSet.has(stones[x])) {
+            count++;
         }
     }
+
     return count;
 };
