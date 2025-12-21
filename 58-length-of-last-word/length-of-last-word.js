7e259d525b5457ccb1c641f2a39862e9d4acfcaa
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2025-12-21 14:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (76.45%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,21 +3,33 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    let length = 0;
-    let isWordFound = false;
+    // let length = 0;
+    // let isWordFound = false;
 
-    // * starting from back of the string
-    for (let x = s.length - 1; x >= 0; x--) {
-        // * checking the char is empty or not empty not then we found a word and we should start counting untill the next empty space come
+    // // * starting from back of the string
+    // for (let x = s.length - 1; x >= 0; x--) {
+    //     // * checking the char is empty or not empty not then we found a word and we should start counting untill the next empty space come
+    //     if (s[x] !== " ") {
+    //         length++;
+    //         isWordFound = true;
+    //     }
+    //     // if word is already found and we encounter a space then simply we should break out of the loop & if word has trailing space and word is not found then loop continues untill the word found
+    //     else if (s[x] == " " && isWordFound) {
+    //         break;
+    //     }
+    // }
+
+    // return length;
+
+    let count = 0;
+
+    for (let x = s.length - 1; x >= 0 ; x--) {
         if (s[x] !== " ") {
-            length++;
-            isWordFound = true;
-        }
-        // if word is already found and we encounter a space then simply we should break out of the loop & if word has trailing space and word is not found then loop continues untill the word found
-        else if (s[x] == " " && isWordFound) {
+            count++;
+        } else if (count > 0) {
             break;
         }
     }
 
-    return length;
+    return count;
 };
