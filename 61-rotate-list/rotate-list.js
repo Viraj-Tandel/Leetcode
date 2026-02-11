c4d9451bd80ccdc03da04a2088177bd4592a9db4
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-02-11 23:42:36 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (76.51%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -11,45 +11,34 @@
  * @return {ListNode}
  */
 var rotateRight = function (head, k) {
-
-    if (!head || !head.next) {
+    if(!head || !head.next)
         return head;
-    }
-
-    let current = head;
-    let slow = head;
-    let fast = head;
     let length = 0;
+    let current = head;
+    let fast = head;
+    let slow = head;
 
-    // calculating length of linked list
     while (current) {
-        current = current.next;
+        current = current.next
         length++;
     }
 
     k = k % length;
 
-    if (k == 0) {
+    if(k==0)
         return head;
-    }
 
-    // keeping the gap of k where we want to break the linked list in two part end & start of linked list
     for (let x = 0; x < k; x++) {
         fast = fast.next;
     }
 
     while (fast.next) {
-        fast = fast.next;
         slow = slow.next;
+        fast = fast.next;
     }
 
-    // storing the next before making it to point out to null
     let newHead = slow.next;
-
-    // keeping the last next to be null
     slow.next = null;
-
-    // end of linked list pointing to head
     fast.next = head;
 
     return newHead;
