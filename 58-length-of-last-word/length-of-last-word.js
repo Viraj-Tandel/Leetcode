d7274abf16daad41ab3caa5c6df3c254c7ff187e
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-02-16 11:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (38.39%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,33 +3,16 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    // let length = 0;
-    // let isWordFound = false;
-
-    // // * starting from back of the string
-    // for (let x = s.length - 1; x >= 0; x--) {
-    //     // * checking the char is empty or not empty not then we found a word and we should start counting untill the next empty space come
-    //     if (s[x] !== " ") {
-    //         length++;
-    //         isWordFound = true;
-    //     }
-    //     // if word is already found and we encounter a space then simply we should break out of the loop & if word has trailing space and word is not found then loop continues untill the word found
-    //     else if (s[x] == " " && isWordFound) {
-    //         break;
-    //     }
-    // }
-
-    // return length;
-
-    let count = 0;
-
-    for (let x = s.length - 1; x >= 0 ; x--) {
+    let cnt = 0;
+    let isCharFound = false;
+    for (let x = s.length - 1; x >= 0; x--) {
         if (s[x] !== " ") {
-            count++;
-        } else if (count > 0) {
+            cnt++;
+            isCharFound = true;
+        } else if (s[x] == " " && isCharFound) {
             break;
         }
     }
 
-    return count;
+    return cnt;
 };
