ca6191aef962125a71b61b703ec9ddfb7be8904e
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-03-14 21:09:49 +0530

    Time: 2 ms (44.67%) | Memory: 55.7 MB (24.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,32 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    // let result = 0;
-    // for (let x = 0; x < strs[0].length; x++) {
-    //     let cnt = 0;
-    //     let temp = strs[0].substring(0, x + 1);
-    //     for (let y = 0; y < strs.length; y++) {
-    //         if (strs[y].substring(0, temp.length) == temp) {
-    //             cnt++;
-    //         }
-    //     }
-    //     if (cnt == strs.length) {
-    //         result++;
-    //     }
-    // }
-
-    // return strs[0].slice(0, result);
     let index = 0;
 
     for (let x = 0; x < strs[0].length; x++) {
         let temp = strs[0][x];
         let isEq = true;
-        for (let y = 0; y < strs.length; y++) {
-            if (temp !== strs[y][x]) {
+
+        for (let y = 1; y < strs.length; y++) {
+            if (x >= strs[y].length || strs[y][x] !== temp) {
                 isEq = false;
                 break;
             }
         }
+
         if (isEq) {
             index++;
         } else {
