9f500f0b9b59df0ad731609ecc35f2eb0b1c9801
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-03-15 14:06:29 +0530

    Time: 9 ms (21.37%) | Memory: 56.8 MB (15.45%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    // let sFreqMap = {};
+    // let tFreqMap = {};
+
+    // for (let x = 0; x < s.length; x++) {
+    //     if (sFreqMap[s[x]]) {
+    //         sFreqMap[s[x]]++;
+    //     } else {
+    //         sFreqMap[s[x]] = 1;
+    //     }
+    // }
+
+    // for (let x = 0; x < t.length; x++) {
+    //     if (tFreqMap[t[x]]) {
+    //         tFreqMap[t[x]]++;
+    //     } else {
+    //         tFreqMap[t[x]] = 1;
+    //     }
+    // }
+
+    // return Object.keys(sFreqMap).length === Object.keys(tFreqMap).length;
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let freqMap = {};
+
+    for (let x = 0; x < s.length; x++) {
+        if (!freqMap[s[x]]) {
+            if (Object.values(freqMap).indexOf(t[x]) == -1) {
+                freqMap[s[x]] = t[x];
+            } else {
+                return false;
+            }
+        } else if (freqMap[s[x]]) {
+            if (freqMap[s[x]] !== t[x]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
