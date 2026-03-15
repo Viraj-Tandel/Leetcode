04f64e831faa7f38f9aceb8462d0bfdc6ea3ecdf
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-03-15 11:08:10 +0530

    Time: 10 ms (81.40%) | Memory: 55.6 MB (76.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,70 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    // if (s.length !== t.length) {
-    //     return false;
-    // }
-
-    // let sMap = {};
-    // let tMap = {};
-
-    // for (let x = 0; x < t.length; x++) {
-    //     if (sMap[s[x]]) {
-    //         sMap[s[x]]++;
-    //     } else {
-    //         sMap[s[x]] = 1;
-    //     }
-
-    //     if (tMap[t[x]]) {
-    //         tMap[t[x]]++;
-    //     } else {
-    //         tMap[t[x]] = 1;
-    //     }
-    // }
-
-    // for (let x = 0; x < t.length; x++) {
-    //     if (tMap[t[x]] && sMap[t[x]]) {
-    //         tMap[t[x]]--;
-    //         sMap[t[x]]--;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
-    // return true;
-
-    // if (s.length !== t.length) {
-    //     return false;
-    // }
-
-    // let freqMap = {};
-
-    // for (let x = 0; x < s.length; x++) {
-    //     freqMap[s[x]] = (freqMap[s[x]] || 0) + 1;
-    // }
-
-    // for (let x = 0; x < t.length; x++) {
-    //     if (!freqMap[t[x]]) return false;
-    //     freqMap[t[x]]--;
-    // }
-
-    // return true;
-
-    // Revision ---> 8/2/25
-    if (s.length !== t.length) {
-        return false;
-    }
-
     let freqMap = {};
 
+    // Counting frequancy
     for (let x = 0; x < s.length; x++) {
-        freqMap[s[x]] = (freqMap[s[x]] || 0) + 1;
+        if (freqMap[s[x]]) {
+            ++freqMap[s[x]];
+        } else {
+            freqMap[s[x]] = 1;
+        }
     }
 
+    // checking frequancy for other string
     for (let x = 0; x < t.length; x++) {
         if (freqMap[t[x]]) {
             freqMap[t[x]]--;
         } else {
+            return false;
+        }
+    }
+
+    for (let x in freqMap) {
+        if (freqMap[x] != 0) {
             return false;
         }
     }
