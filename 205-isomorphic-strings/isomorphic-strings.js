9ba919eca145f55ca82049043a111064dfe6812b
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-03-15 14:48:01 +0530

    Time: 11 ms (11.41%) | Memory: 55.7 MB (31.59%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -24,23 +24,45 @@ var isIsomorphic = function (s, t) {
     // }
 
     // return Object.keys(sFreqMap).length === Object.keys(tFreqMap).length;
+
+
+    // if (s.length !== t.length) {
+    //     return false;
+    // }
+
+    // let freqMap = {};
+
+    // for (let x = 0; x < s.length; x++) {
+    //     if (!freqMap[s[x]]) {
+    //         if (Object.values(freqMap).indexOf(t[x]) == -1) {
+    //             freqMap[s[x]] = t[x];
+    //         } else {
+    //             return false;
+    //         }
+    //     } else if (freqMap[s[x]]) {
+    //         if (freqMap[s[x]] !== t[x]) {
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    // return true;
+
     if (s.length !== t.length) {
         return false;
     }
 
-    let freqMap = {};
+    let sToTmap = {};
+    let tToSmap = {};
 
     for (let x = 0; x < s.length; x++) {
-        if (!freqMap[s[x]]) {
-            if (Object.values(freqMap).indexOf(t[x]) == -1) {
-                freqMap[s[x]] = t[x];
-            } else {
-                return false;
-            }
-        } else if (freqMap[s[x]]) {
-            if (freqMap[s[x]] !== t[x]) {
-                return false;
-            }
+        if (!sToTmap[s[x]] && !tToSmap[t[x]]) {
+            sToTmap[s[x]] = t[x];
+            tToSmap[t[x]] = s[x];
+        } else if (sToTmap[s[x]] !== t[x]) { // if already exist inside S to T map (foo --> bar)
+            return false;
+        } else if (tToSmap[t[x]] !== s[x]) { // if already exist inside T to S map (bar --> foo)
+            return false;
         }
     }
 
