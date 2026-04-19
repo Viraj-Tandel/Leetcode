195a74611c16d9093a323d1ade84c40314ab37d8
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-04-19 11:07:41 +0530

    Time: 84 ms (9.79%) | Memory: 64.9 MB (8.90%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -5,31 +5,52 @@
 var evalRPN = function (tokens) {
     let stack = [];
 
+    let map = {
+        "+": (a, b) => (b + a),
+        "-": (a, b) => (b - a),
+        "/": (a, b) => (b / a),
+        "*": (a, b) => (b * a),
+    }
+
+    // for (let x = 0; x < tokens.length; x++) {
+    //     if (tokens[x] == '+' || tokens[x] == '-' || tokens[x] == '/' || tokens[x] == '*') {
+    //         // when operator comes
+    //         let op2 = stack.pop();
+    //         let op1 = stack.pop();
+    //         let result;
+
+    //         switch (tokens[x]) {
+    //             case '+':
+    //                 result = op1 + op2;
+    //                 break;
+
+    //             case '-':
+    //                 result = op1 - op2;
+    //                 break;
+
+    //             case '/':
+    //                 result = op1 / op2;
+    //                 break;
+
+    //             case '*':
+    //                 result = op1 * op2;
+    //                 break;
+    //         }
+
+    //         stack.push(Math.trunc(result));
+    //     } else {
+    //         // This any number
+    //         stack.push(Number(tokens[x]));
+    //     }
+    // }
+
     for (let x = 0; x < tokens.length; x++) {
-        if (tokens[x] == '+' || tokens[x] == '-' || tokens[x] == '/' || tokens[x] == '*') {
-            // when operator comes
+
+        if (map[tokens[x]]) {
             let op2 = stack.pop();
             let op1 = stack.pop();
-            let result;
-
-            switch (tokens[x]) {
-                case '+':
-                    result = op1 + op2;
-                    break;
-
-                case '-':
-                    result = op1 - op2;
-                    break;
-
-                case '/':
-                    result = op1 / op2;
-                    break;
-
-                case '*':
-                    result = op1 * op2;
-                    break;
-            }
-
+            let result = map[tokens[x]](op2, op1);
+            console.log(result);
             stack.push(Math.trunc(result));
         } else {
             // This any number
