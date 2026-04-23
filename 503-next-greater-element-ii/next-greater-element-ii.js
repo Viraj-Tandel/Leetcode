7d2cb60853d52681c3840bf8c154cbc1114e4d13
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-04-23 21:33:55 +0530

    Time: 5 ms (85.46%) | Memory: 61.3 MB (79.68%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -2,22 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var nextGreaterElements = function (nums) {
-    const arr = [...nums, ...nums];
+var nextGreaterElements = function (arr) {
     let result = new Array(arr.length).fill(-1);
     let n = arr.length;
     let stack = [];
 
-    for (let x = n - 1; x >= 0; x--) {
-        let currentEle = arr[x];
+    for (let x = (n * 2) - 1; x >= 0; x--) {
+        let currentEle = arr[x % n];
         while (stack.length > 0 && stack[stack.length - 1] <= currentEle) {
             stack.pop();
         }
         if (stack.length > 0) {
-            result[x] = stack[stack.length - 1];
+            result[x % n] = stack[stack.length - 1];
         }
         stack.push(currentEle);
     }
 
-    return result.slice(0, nums.length);
+    return result;
 };
