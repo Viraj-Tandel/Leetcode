b517f3ea4ec4037d47fcd6bfd9c2dcb3eb8377d1
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-04-30 00:28:07 +0530

    Time: 59 ms (5.82%) | Memory: 53.8 MB (30.59%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -18,38 +18,21 @@ var solution = function (isBadVersion) {
      * @return {integer} The first bad version
      */
     return function (n) {
+        let l = 1;
+        let r = n;
+        let badVersion;
 
-        // let left = 1;
-        // let right = n;
-        // let mid;
+        while (l <= r) {
+            let m = l + Math.floor((r - l) / 2);
 
-        // while (left < right) {
-        //     mid = Math.floor((left + right) / 2);
-        //     let result = isBadVersion(mid);
-
-        //     if (!result) {
-        //         left = mid + 1;
-        //     } else {
-        //         right = mid;
-        //     }
-        // }
-
-        // return left;
-
-        // * Practice solve 7/8/2025
-
-        let left = 1;
-        let right = n;
-
-        while (left < right) {
-            let mid = Math.floor((left + right) / 2);
-
-            if (isBadVersion(mid)) {
-                right = mid;
+            if (isBadVersion(m)) {
+                badVersion = m;
+                r = m - 1;
             } else {
-                left = mid + 1
+                l = m + 1;
             }
         }
-        return left;
+
+        return badVersion;
     };
 };
