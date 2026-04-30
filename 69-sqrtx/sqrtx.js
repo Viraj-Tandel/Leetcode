1058cecd4cae9265bf3b023a992f0fc1fe29ae97
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-04-30 16:05:26 +0530

    Time: 1 ms (73.85%) | Memory: 57.1 MB (19.91%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -2,26 +2,29 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {
-    if (x < 2) {
+var mySqrt = function(x) {
+    if( x < 2){
         return x;
     }
 
-    let left = 2;
-    let right = Math.floor(x / 2);
+    let l = 2;
+    let r = Math.floor(x/2);
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let square = mid * mid;
+    while(l <= r){
+        let m = l + Math.floor((r - l)/2);
+        let square = m * m;
 
-        if (square > x) {
-            right = mid - 1;
-        } else if (square < x) {
-            left = mid + 1;
-        } else {
-            return mid;
+        if(square == x){
+            return m;
+        }
+
+        // it means we need to move in right as there is more nearer ans exsit
+        if(square < x){
+            l = m + 1;
+        } else{
+            r = m - 1;
         }
     }
 
-    return right;
+    return r;
 };
