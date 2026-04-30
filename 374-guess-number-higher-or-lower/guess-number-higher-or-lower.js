158009a78e592ff95e7d9d1e95010de1846a155c
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-04-30 14:54:33 +0530

    Time: 52 ms (9.34%) | Memory: 53 MB (86.49%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
@@ -12,20 +12,26 @@
  * @return {number}
  */
 var guessNumber = function (n) {
-    let left = 1;
-    let right = n;
+    let l = 1;
+    let r = n;
 
-    while (left <= right) {
-        let middle = left + Math.floor((right - left) / 2);
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
 
-        if (guess(middle) == -1) {
-            right = middle - 1;
-        } else if (guess(middle) == 1) {
-            left = middle + 1;
-        } else {
-            return middle;
+        // mid is higher than the picked number so reducing search space towards left
+        if (guess(m) == -1) {
+            r = m - 1;
+        }
+        // mid is lower than the picked number so reducing search space towards right
+        else if (guess(m) == 1) {
+            l = m + 1;
+        }
+        // mid is the picked number
+        else {
+            return m;
         }
     }
 
+    // as per the problem there always a answer exist but just return as per problem return type
     return -1;
 };
