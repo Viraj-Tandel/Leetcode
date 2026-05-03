b2452187775c2f484a40f9ab63e69d638d7f66e7
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-05-03 22:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (40.48%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,32 +4,28 @@
  * @return {number}
  */
 var search = function (a, target) {
+    // REVISED 2nd Time (3/5/26)
+
     let l = 0;
     let r = a.length - 1;
 
     while (l <= r) {
-        let m = l + Math.floor((r - l) / 2);
+        let m = Math.floor(l + (r - l) / 2);
 
-        // return index if target is at mid index
         if (a[m] == target) {
             return m;
         }
 
-        // when left side is sorted
         if (a[l] <= a[m]) {
             if (target >= a[l] && target < a[m]) {
-                // move right pointer
                 r = m - 1;
             } else {
-                // this means target exsit in another unsorted part
                 l = m + 1;
             }
         } else {
-            // exist in right half
             if (target > a[m] && target <= a[r]) {
                 l = m + 1;
             } else {
-                // exisit towrds the left side of right half
                 r = m - 1;
             }
         }
