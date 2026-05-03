968c97e3415d19b98a9b323f9da85234d7d898b2
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-05-03 22:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (86.31%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -9,12 +9,13 @@ var findPeakElement = function (a) {
     while (l < r) {
         let m = l + Math.floor((r - l) / 2);
 
-        if (a[m + 1] > a[m]) {
+        if (a[m] < a[m + 1]) {
             l = m + 1;
         } else {
             r = m;
         }
     }
 
+    // retunr l/r both are on same index
     return l;
 };
