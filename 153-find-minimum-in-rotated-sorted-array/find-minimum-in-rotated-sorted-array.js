e033c35fccee521d48e9606220914e2740ce9fb2
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-05-04 17:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (38.76%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,26 +3,26 @@
  * @return {number}
  */
 var findMin = function (a) {
-
     let l = 0;
     let r = a.length - 1;
 
     while (l <= r) {
-        // EDGE CASE ARRAY ALREADY SORTED
         if (a[l] <= a[r]) {
             return a[l];
         }
+
         let m = l + Math.floor((r - l) / 2);
 
-        // Mid is rotation point if less than its previous elment like [7 0 1 2 3...] 
         if (a[m] < a[m - 1]) {
             return a[m];
         }
-        // Left part is sorted then right side is unsorted sp answer lies in the right part
-        if (a[l] <= a[m]) {
-            l = m + 1;
-        } else {
+
+        if (a[m] < a[r]) {
             r = m - 1;
+        } else {
+            l = m + 1;
         }
+
     }
+
 };
