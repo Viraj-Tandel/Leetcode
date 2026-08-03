f253510938df42d1bcaa86fcda97a0fadcde4f34
its_viru_03 <88768356+Viraj-Tandel@users.noreply.github.com>
2026-08-03 22:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (54.74%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,27 +3,27 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (a, target) {
-    // REVISED 2nd Time (3/5/26)
-
+var search = function (arr, target) {
     let l = 0;
-    let r = a.length - 1;
+    let r = arr.length - 1;
 
     while (l <= r) {
-        let m = Math.floor(l + (r - l) / 2);
+        let m = l + Math.floor((r - l) / 2);
 
-        if (a[m] == target) {
+        if (arr[m] == target) {
             return m;
         }
 
-        if (a[l] <= a[m]) {
-            if (target >= a[l] && target < a[m]) {
+        if (arr[l] <= arr[m]) {
+            // LEFT SIDE IS SORTED
+            if (target >= arr[l] && target < arr[m]) {
                 r = m - 1;
             } else {
                 l = m + 1;
             }
         } else {
-            if (target > a[m] && target <= a[r]) {
+            // RIGHT SIDE IS SORTED
+            if (target > arr[m] && target <= arr[r]) {
                 l = m + 1;
             } else {
                 r = m - 1;
